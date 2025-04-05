7a95de181fb5e077c9c5a019c7134828ff12fb08
agastyaa-nar <agastyadhanar@gmail.com>
2025-04-05 22:03:52 +0700

    prep

---
+++ quiz/Quiz-Array-Dimension/soal1.js
@@ -0,0 +1,47 @@
+/*
+=====================
+DOUBLE REVERSE ARRAY
+=====================
+
+[INSTRUCTION]
+Terdapat function doubleReverse yang menerima parameter berupa array of string, 
+function ini akan memutar elemen array, string yang berada di dalam elemen array juga akan dibalik atau diputar jika panjang string GENAP
+
+[EXAMPLE]
+input: ['rabu', 'cinta', 'benci', 'masuk', 'nikmat']
+proses: memutar isi array, dan memutar elemen array yang panjang katanya genap
+output: [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+
+[RULES]
+- Function bawaan javascript yang boleh digunakan hanyalah .push dan .length
+- DILARANG menggunakan .reverse()
+*/
+
+function doubleReverse(arr) {
+    //code here 
+    if (arr.length === 0) return "Invalid Input Parameter";
+
+    let hasil = [];
+    for(let i = arr.length - 1 ; i >= 0 ;i--) {
+        let kata = arr[i];
+        if (kata.length % 2 === 0) {
+            let reverse = '';
+            for (let j = kata.length - 1; j >= 0;j--) {
+                reverse += kata[j];
+            }
+            hasil.push(reverse);
+        } else {
+            hasil.push(kata);
+        }
+    }
+    return hasil;
+}
+
+console.log(doubleReverse(['rabu', 'cinta', 'benci', 'masuk', 'nikmat']));
+// [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+console.log(doubleReverse(['aku', 'sayang', 'kamu']));
+// [ 'umak', 'gnayas', 'aku' ]
+console.log(doubleReverse(['pelakor', 'perusak', 'rumah', 'tangga']));
+//[ 'anggnat', 'rumah', 'perusak', 'pelakor' ]
+console.log(doubleReverse([]))
+// invalid input parameter
